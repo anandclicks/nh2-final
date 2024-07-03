eedd00a15db1a4ed04e1c022299a3353c862bd6d
anandclicks <146056018+anandclicks@users.noreply.github.com>
2024-07-03 14:00:50 +0530

    fw

---
+++ js/apiCall.js
@@ -0,0 +1,55 @@
+const queryForm = document.querySelectorAll("#queryForm");
+
+queryForm.forEach(e=> {
+  e.addEventListener("submit", async (evt) => {
+    evt.preventDefault();
+
+    const name = document.querySelector("#name").value;
+    const mobileNumber = document.querySelector("#mobileNumber").value;
+    const propertyName = document.querySelector("#propertyName").value;
+    const roomSharing = document.querySelector("#roomSharing").value;
+
+    let userInfo = {
+        name,
+        mobileNumber,
+        propertyName,
+        roomSharing
+    };
+
+    console.log(userInfo);
+
+    document.querySelector(".loadingpage").style.display = "flex";
+    document.querySelector(".sending").style.display = "flex";
+
+    try {
+        const response = await fetch("https://backend-for-nh2.onrender.com", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(userInfo)
+        });
+
+        if (response.ok) {
+            document.querySelector(".sending").style.display = "none";
+            document.querySelector(".send").style.display = "flex";
+
+            setTimeout(() => {
+                document.querySelector(".loadingpage").style.display = "none";
+                document.querySelector(".sending").style.display = "none";
+                document.querySelector(".send").style.display = "none";
+
+                // Reset form fields (example function calls)
+               form.classList.add("dnone") 
+      form.classList.remove("visible")
+            }, 4000);
+        } else {
+            throw new Error('Failed to submit form');
+        }
+    } catch (error) {
+        console.error('Error submitting form:', error);
+        // Handle error: display an error message or retry logic
+    }
+});
+
+})
